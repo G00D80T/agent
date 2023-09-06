fe0428392a47d0073d0d55574e54a4350d8619fd
G00D80T <1maxgulov@gmail.com>
2023-09-06 22:28:30 +0300

    sout "чётное\нечётное" + "число ="

---
+++ Task_1.java
@@ -0,0 +1,15 @@
+package ru.mpei.agent.task_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    double b = 2.0;
+
+    if (b % 2 == 0) {
+        System.out.println("ЧЁТНОЕ");
+    }else{
+        System.out.println("НЕЧЁТНОЕ");
+    }
+    System.out.println("число = " +b);
+}
+}
